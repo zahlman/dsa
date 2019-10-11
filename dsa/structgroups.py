0c525ea2ef91385ca42a37727932c48855b7299c
Karl Knechtel <karl.a.knechtel@gmail.com>
2019-10-11 01:38:33 -0400

    Refactor logic for terminating the chunk disassembly process

--- dsa/structgroups.py
+++ dsa/structgroups.py
@@ -8,24 +8,16 @@ class UNRECOGNIZED_FOLLOWERS(UserError):
     """unrecognized followers `{extra}` for struct `{current}`"""
 
 
-class MISSING_TERMINATOR_NATURAL(UserError):
-    """missing terminator after `{previous}` struct"""
+class CHUNK_END_CONFLICT(UserError):
+    """end of chunk may be specified in at most one way (terminator sequence, `last` struct or explicit count of structs)"""
 
 
-class MISSING_CHUNKS(UserError):
-    """premature end of chunk; {remaining} struct(s) missing"""
-
-
-class MISSING_TERMINATOR_COUNTED(UserError):
-    """missing terminator after {count} struct(s)"""
+class CHUNK_LOADING_FAILED(UserError):
+    """couldn't load chunk ({reason})"""
 
 
 class NO_MATCH(SequenceError):
     """couldn't match a struct at chunk offset 0x{offset:X}"""
-
-
-class CHUNK_TOO_BIG(UserError):
-    """too many structs in chunk"""
 
 
 # Doesn't quite fit the pattern for a MappingError.
@@ -33,8 +25,8 @@ class INVALID_FOLLOWER(UserError):
     """struct `{name}` invalid here (valid options: {followers})"""
 
 
-class CHUNK_TOO_SMALL(UserError):
-    """not enough structs in chunk"""
+class BAD_CHUNK_SIZE(UserError):
+    """chunk has {actual} structs; exactly {required} required"""
 
 
 _struct_name_parser = line_parser(
@@ -67,11 +59,12 @@ class StructGroup:
         self._count = options.count # number of structs, if exact count required
         self._terminator = options.terminator
         self._graph = _normalized_graph(graph, options.first)
-        self._implicit_end = (
-            self._count is None and
-            self._terminator is None and
-            all(self._graph.values()) # no last-in-chunk structs
-        )
+        counted = self._count is not None
+        terminated = self._terminator is not None
+        has_last = not all(self._graph.values())
+        end_methods = int(terminated) + int(has_last) + int(counted)
+        CHUNK_END_CONFLICT.require(end_methods <= 1)
+        self._expect_termination = terminated or has_last
 
 
     @property
@@ -79,42 +72,12 @@ class StructGroup:
         return self._alignment
 
 
-    def _at_end(self, data, offset):
+    def _terminator_amount(self, data, loc):
         t = self._terminator
-        return False if t is None else (t == data[offset:offset+len(t)])
+        return 0 if t is None else len(t) if data[loc:loc+len(t)] == t else 0
 
 
-    def _candidates(self, data, offset, previous, count):
-        unterminated = self._terminator is None
-        if self._at_end(data, offset):
-            # N.B. If there are `last` structs in the group and we didn't
-            # reach one, this is *not* considered an error.
-            result = set()
-        else:
-            result = self._graph[previous]
-            MISSING_TERMINATOR_NATURAL.require(
-                bool(result) or unterminated, previous=previous
-            )
-        # Validate against count, if applicable.
-        remaining = None if self._count is None else self._count - count
-        if not result:
-            MISSING_CHUNKS.require(
-                remaining in {0, None}, remaining=remaining
-            )
-        elif remaining == 0:
-            MISSING_TERMINATOR_COUNTED.require(unterminated, count=count)
-            # Otherwise: end of counted block, and last struct was not `last`;
-            # this is not an error, but we stop formatting here.
-            result = set()
-        return result
-
-
-    def _extract(self, data, offset, previous, count, chunk_label):
-        if self._implicit_end and len(data) <= offset:
-            return None # implicitly ended at end of data.
-        candidates = self._candidates(data, offset, previous, count)
-        if not candidates:
-            return None
+    def _extract(self, candidates, data, offset, chunk_label):
         # name, match, referents, size
         # referents is a list of (group name, location, label_base) tuples
         return NO_MATCH.first_not_none((
@@ -130,8 +93,7 @@ class StructGroup:
         ))
 
 
-    def _parse(self, tokens, count, previous):
-        CHUNK_TOO_BIG.require(self._count is None or count < self._count)
+    def _parse(self, tokens, previous):
         followers = self._graph[previous]
         # Name extraction can't fail, since empty lines are skipped.
         name, tokens = _struct_name_parser(tokens)
@@ -142,15 +104,21 @@ class StructGroup:
 
 
     def _parse_end(self, count):
-        CHUNK_TOO_SMALL.require(self._count is None or count >= self._count)
+        BAD_CHUNK_SIZE.require(
+            self._count is None or count == self._count,
+            actual=count,
+            required=self._count
+        )
         return b'' if self._terminator is None else self._terminator
 
 
     def assemble(self, lines):
         previous = None
         result = bytearray()
-        for i, line in enumerate(lines):
-            previous, data = self._parse(line, i, previous)
+        for i, line in enumerate(lines, 1):
+            previous, data = wrap_errors(
+                f'struct #{i}', self._parse, line, previous
+            )
             result.extend(data)
         result.extend(self._parse_end(len(lines)))
         return bytes(result)
@@ -163,24 +131,60 @@ class StructGroup:
             return 0
 
 
+    def _understand_failure(self, i):
+        if self._expect_termination:
+            return "didn't find a valid terminator sequence or `last` chunk"
+        if self._count is None:
+            return "couldn't parse struct #{i}"
+        return "couldn't parse struct {i}/{self._count}"
+
+
+    def _progress(self, i):
+        return f'{i}/{self._count}' if self._count is not None else f'#{i}'
+
+
+    def _candidates(self, data, offset, previous):
+        read_to_end = self._count is None and not self._expect_termination
+        if offset == len(data) and read_to_end:
+            # consumed the entire block without other termination; success.
+            return 0, None
+        amount = self._terminator_amount(data, offset)
+        if amount != 0:
+            # reached a valid terminator; success.
+            return amount, None
+        return 0, self._graph[previous]
+        # if there are no candidates, reached a valid `last` struct; success.
+
+
     # Get the disassembled lines for a chunk and the corresponding chunk size.
     def disassemble(self, config, chunk_label, data, register, label_ref):
-        # TODO use `config` to pass in a struct count if applicable.
-        struct_name = None
+        # `config` is ignored.
+        previous = None
         offset = 0
         lines = []
-        for i in count():
-            result = self._extract(data, offset, struct_name, i, chunk_label)
-            if result is None:
-                total_size = offset
-                if self._terminator is not None:
-                    total_size += len(self._terminator)
-                return total_size, lines
+        enumerator = (
+            range(1, self._count+1)
+            if self._count is not None
+            else count(1)
+        )
+        for i in enumerator:
+            adjustment, candidates = self._candidates(data, offset, previous)
+            if not candidates:
+                offset += adjustment
+                break
+            result = self._extract(candidates, data, offset, chunk_label)
+            CHUNK_LOADING_FAILED.require(
+                result is not None,
+                reason=self._understand_failure(i)
+            )
             struct_name, match, referents, struct_size = result
-            offset += struct_size
             for referent in referents:
                 register(*referent)
             lines.append(self._format(
-                f'Struct {struct_name} (at 0x{offset:X})',
+                f'Struct {struct_name} ({self._progress(i)})',
                 struct_name, match, label_ref
             ))
+            offset += struct_size
+            previous = struct_name
+        assert self._count in {None, i}
+        return offset, lines
