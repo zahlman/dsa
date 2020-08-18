26e4b869dcc7a8abf0421c6ab572731749ea606a
Karl Knechtel <karl.a.knechtel@gmail.com>
2020-08-17 23:14:47 -0400

    add new setup for timing and tracing

--- dsa/ui/tracing.py
+++ dsa/ui/tracing.py
@@ -8,6 +8,54 @@ from time import time
 """A simple logging system with timing and indentation."""
 
 
+class _Helper:
+    # An implementation class that can be used as either a decorator
+    # or a context manager.
+    def __init__(self, owner, message):
+        self._owner = owner
+        self._message = message
+
+
+    def __enter__(self):
+        return self._owner.start(self._message)
+
+
+    def __exit__(self, exc_type, exc, traceback):
+        self._owner.stop('Aborted' if exc else 'Done')
+
+
+    def __call__(self, func):
+        @wraps(func)
+        def _wrapped(*args, **kwargs):
+            with self:
+                return func(*args, **kwargs)
+        return _wrapped
+
+
+class TimingTracer:
+    def __init__(self):
+        self._timestamps = []
+
+
+    def trace(self, message):
+        print('    ' * len(self._timestamps), message, sep='')
+
+
+    def start(self, message):
+        self.trace(f'{message}...')
+        self._timestamps.append(time())
+        return self
+
+
+    def stop(self, tag):
+        elapsed = int((time() - self._timestamps.pop()) * 1000)
+        self.trace(f'{tag} ({elapsed} ms)')
+
+
+    def __call__(self, message):
+        return _Helper(self, message)
+
+
 def trace(message):
     if trace._tracing:
         print('    ' * trace._level + message)
